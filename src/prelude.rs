4f126202c7681e7611197ee8a4c7ec5daae68fdb
Peter Bjorklund <piot@hotmail.com>
2025-03-29 20:03:33 +0100

    chore: move from old repo

---
+++ src/prelude.rs
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/eira
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+pub use crate::{
+    Color, FileSpanMessage, Header, Kind, Label, Pos, PosSpan, Scope, SourceFileSection,
+    SourceLines,
+};
